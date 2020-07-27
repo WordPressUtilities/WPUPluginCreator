bb7e44b3f47316a005686accb0ce4cf2ea44c881
Kevin <kevin@rocher-dev.com>
2020-07-27 22:49:47 +0200

    v 0.6.1
    * Prevent directory listing with a « silence is golden » index.php

--- bin/create/file.sh
+++ bin/create/file.sh
@@ -8,7 +8,9 @@ echo '# Create plugin file';
 mkdir "${_PLUGIN_DIR}";
 cp "${_TOOLSDIR}skeleton.php" "${_PLUGIN_FILE}";
 
-
 echo '# Create README';
 cp "${_TOOLSDIR}README.md" "${_PLUGIN_DIR}README.md";
 wpuplugincreator_set_values "${_PLUGIN_DIR}README.md";
+
+echo '# Create other files';
+echo "<?php // Silence is Golden " > "${_PLUGIN_DIR}index.php";
