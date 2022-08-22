105fff1ba021294a301d1aa8c3926685067d5e8f
Kevin <kevin@rocher-dev.com>
2022-08-22 19:15:57 +0200

    v 0.11.2
    - Fix default plugin classname.

--- bin/questions.sh
+++ bin/questions.sh
@@ -29,6 +29,7 @@ plugin_id=$(bashutilities_get_user_var "- What is the plugin ID ?" "${default_pl
 default_plugin_classname=$(bashutilities_titlecase "${plugin_name// /}");
 default_plugin_classname="${default_plugin_classname//-/}";
 plugin_classname=$(bashutilities_get_user_var "- What is the plugin ClassName ?" "${default_plugin_classname}");
+default_plugin_classname="${default_plugin_classname//[^[:alnum:]]/}";
 
 ###################################
 ## Translation
