332f10ec51aeb96770a72e459fb13f0bfaecac85
Kevin <kevin@rocher-dev.com>
2023-05-22 10:13:23 +0200

    v 0.18.2
    - Update base plugin.

--- wpuplugincreator.sh
+++ wpuplugincreator.sh
@@ -2,7 +2,7 @@
 
 WPUPluginCreator(){
 
-local _WPUPLUGINCREATOR_VERSION='0.18.1';
+local _WPUPLUGINCREATOR_VERSION='0.18.2';
 local _SOURCEDIR="$( dirname "${BASH_SOURCE[0]}" )/";
 local _TOOLSDIR="${_SOURCEDIR}sources/";
 local _CURRENT_DIR="$( pwd )/";
