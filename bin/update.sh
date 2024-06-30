94d5bb0bef20fe4c043e897122ec92cf276e3500
Kevin <kevin@rocher-dev.com>
2024-06-30 23:01:12 +0200

    v 0.42.1
    - Update github actions when migrating branches.

--- bin/update.sh
+++ bin/update.sh
@@ -330,6 +330,7 @@ function wpuplugincreator_migrate_from_master_to_main(){
     if [[ "${_migrate_branch}" == 'y' ]];then
         git add .;
         git branch -m master main;
+        wpuplugincreator_update_github_actions;
         local _remote_github_base=$(wpuplugincreator_get_github_repo_url);
         bashutilities_message  "- The current branch is now 'main'." 'success';
         bashutilities_message "Do not forget to change the default branch in the repository settings: ${_remote_github_base}/settings/branches" 'warning';
