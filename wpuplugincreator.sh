070c8bb086e54f8c2a763fb8c86b372c9f3080fd
Kevin <kevin@rocher-dev.com>
2024-07-05 15:57:28 +0200

    v 0.42.3
    - New eslint rules for github actions.

--- wpuplugincreator.sh
+++ wpuplugincreator.sh
@@ -2,7 +2,7 @@
 
 WPUPluginCreator(){
 
-local _WPUPLUGINCREATOR_VERSION='0.42.2';
+local _WPUPLUGINCREATOR_VERSION='0.42.3';
 local _SOURCEDIR="$( dirname "${BASH_SOURCE[0]}" )/";
 local _TOOLSDIR="${_SOURCEDIR}sources/";
 local _CURRENT_DIR="$( pwd )/";
