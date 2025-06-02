e7d9f03c1ee2b08914559e88587c9e82d0883316
Kevin <kevin@rocher-dev.com>
2025-06-02 23:14:46 +0200

    v 0.46.4
    Add : handle github actions in submodules

--- bin/add.sh
+++ bin/add.sh
@@ -48,7 +48,7 @@ for i in "${_DEPENDENCY_LIST[@]}"; do
     wpuplugincreator_add_dependency "${i}";
 done;
 
-if [[ -f ".git/config" ]];then
+if [[ -f ".git/config" || -f ".git" ]];then
     wpuplugincreator_has_github_actions=$(bashutilities_get_yn "- Install Github Actions ?" 'y');
     if [[ "${wpuplugincreator_has_github_actions}" == 'y' ]];then
         wpuplugincreator_create_github_actions;
