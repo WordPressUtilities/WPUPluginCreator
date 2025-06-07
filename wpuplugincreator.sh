c6ce91628a0eaca9974facd1fdb30c809ee2d143
Kevin <kevin@rocher-dev.com>
2025-06-07 23:10:14 +0200

    v 0.46.5
    - Update dependencies

--- wpuplugincreator.sh
+++ wpuplugincreator.sh
@@ -2,7 +2,7 @@
 
 WPUPluginCreator(){
 
-local _WPUPLUGINCREATOR_VERSION='0.46.4';
+local _WPUPLUGINCREATOR_VERSION='0.46.5';
 local _SOURCEDIR="$( dirname "${BASH_SOURCE[0]}" )/";
 local _TOOLSDIR="${_SOURCEDIR}sources/";
 local _CURRENT_DIR="$( pwd )/";
